ab280f7eb0f76b75d6b2adb343da6374f93cbe6e
Alok Singh <aloksingh36757@gmail.com>
2024-11-28 09:49:20 +0530

    strings Program started

---
+++ IsAnagram.java
@@ -0,0 +1,49 @@
+public class IsAnagram{
+
+
+	
+
+	public static void main(String[] args){
+	String s = "silen";
+	String t = "listen";
+	
+	boolean res = isAnagram(s,t);
+
+System.out.println(res);
+		}
+
+	public static boolean isAnagram(String s,String t){
+
+			int[] freq1= getFrequency(s);
+			int[] freq2 = getFrequency(t);
+
+			for(int i =0; i<freq1.length;i++){
+						if(freq1[i] != freq2[i]){
+							return false;	
+						}
+
+			}
+	return true;
+	
+	}
+
+	public static int[] getFrequency(String s1){
+	
+		int[] freq = new int[26];
+
+		for(int i =0;i<s1.length();i++){
+			char c = s1.charAt(i);
+			if(c>=65 && c<=90){
+				freq[c-65]++;
+
+			}
+		else if (c>=97 && c<=122){
+
+				freq[c-97]++;
+			}
+				
+		}
+	return freq;
+	}
+
+}
---
+++ IsPangram.java
@@ -0,0 +1,57 @@
+class IsPangram{
+    public static void main(String[] args) {
+        //String sentence = "thequickbrownfoxjumpsoverthelazydog";
+			
+		String sentence = "leetcode";
+
+   		boolean res = isPangram(sentence);
+			if(res == true){
+
+					System.out.println("it is a  Pangram");
+			}
+		else{
+					System.out.println("It is not a Panagram");
+			}
+			 	
+
+    }
+
+
+public static boolean isPangram(String sentence){
+int[] freq = getFrequency(sentence);
+
+		for(int i =0 ;i<freq.length;i++){
+
+
+						if(freq[i] ==0){
+									return false;
+							}
+		}
+		return true;
+
+
+
+
+}
+
+    public static int[] getFrequency(String s){
+		
+		int[] freq = new int[26];
+		for(int i =0;i<s.length();i++){
+				char c = s.charAt(i);
+	
+			if(c>=65 && c<=90){
+						freq[c-65]++;
+
+				}
+			else if (c>=97 && c<=122){
+							freq[c-97]++;
+					}
+						
+			}
+
+		return freq;
+       
+    }
+
+}
