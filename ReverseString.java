bfbb32de4af75202f58c8cf4f19d910086111bd0
Alok Singh <aloksingh36757@gmail.com>
2024-12-14 16:37:36 +0530

    string programs

--- ReverseString.java
+++ ReverseString.java
@@ -4,11 +4,12 @@ class ReverseString{
 	public static void main(String[] args){
 
 		String s1 = "Mohan and Sohan is here";
-
+		//method calling
 		reverseString(s1);
 	
 
 	}
+	//method declaration
 public static void reverseString(String s){
 					String rev = "";
 				for(int i =s.length()-1;i>=0;i--){
