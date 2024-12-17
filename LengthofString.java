b084dcae5961c2b09928307d08532600a16fb923
Alok Singh <aloksingh36757@gmail.com>
2024-12-17 20:05:29 +0530

    string updated

--- LengthofString.java
+++ LengthofString.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-public class LengthofString{
 
-	public static void main(String[] args){
-			//method calling
-			Scanner sc = new Scanner(System.in);
+public class LengthofString {
+
+	public static void main(String[] args) {
+		// method calling
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String");
 
-			String s = sc.nextLine();
-				lengthOfString(s);
-	
-		
-sc.close();
-		}
-	public static void lengthOfString(String s){
-		char[] a= s.toCharArray();
+		String s = sc.nextLine();
+		lengthOfString(s);
 
-		System.out.println("the length of String "+a.length);
+		sc.close();
+	}
+
+	public static void lengthOfString(String s) {
+		char[] a = s.toCharArray();
+
+		System.out.println("the length of String " + a.length);
 	}
 
 }
--- Variable1.java
+++ Variable1.java
@@ -1,10 +1,12 @@
 class Variable1{
 static int x =5;
 public static void main(String[] args){
+	//demo method calling
 
 		demo();
 	}
 
+	//demo method declaration
 public static void demo(){
 
 		int x =55;
